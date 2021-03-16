66e22d1954ec30ade7c9404287f711c8247b9867
Curtis Jennings Schofield <cschofield@pagerduty.com>
2021-03-15 18:47:58 -0700

    Revert "guess - the interface union is overlapping in the case clause?"
    This reverts commit 19eab089f5715cd2706b131e8624a007a964f4aa.

--- src/main/kotlin/io/curity/dynamoDBDataAccessProvider/DynamoDBClient.kt
+++ src/main/kotlin/io/curity/dynamoDBDataAccessProvider/DynamoDBClient.kt
@@ -66,6 +66,14 @@ class DynamoDBClient(private val config: DynamoDBDataAccessProviderDataAccessPro
 
         if (accessMethod.isEC2InstanceProfile.isPresent && accessMethod.isEC2InstanceProfile.get()) {
             creds = InstanceProfileCredentialsProvider.builder().build()
+        } else if (accessMethod.accessKeyIdAndSecret.isPresent) {
+            val keyIdAndSecret = accessMethod.accessKeyIdAndSecret.get()
+            creds = StaticCredentialsProvider.create(AwsBasicCredentials.create(keyIdAndSecret.accessKeyId.get(), keyIdAndSecret.accessKeySecret.get()))
+
+            /* roleARN is present, get temporary credentials through AssumeRole */
+            if (keyIdAndSecret.awsRoleARN.isPresent) {
+                creds = getNewCredentialsFromAssumeRole(creds, keyIdAndSecret.awsRoleARN.get())
+            }
         } else if (accessMethod.aWSProfile.get().awsProfileName.isPresent) {
             val awsProfile = accessMethod.aWSProfile.get()
             creds = ProfileCredentialsProvider.builder()
@@ -85,20 +93,16 @@ class DynamoDBClient(private val config: DynamoDBDataAccessProviderDataAccessPro
             if (aWSDirect.awsRoleARN.isPresent) {
                 creds = getNewCredentialsFromAssumeRole(creds, aWSDirect.awsRoleARN.get())
             }
-        } else if (accessMethod.accessKeyIdAndSecret.isPresent) {
-            val keyIdAndSecret = accessMethod.accessKeyIdAndSecret.get()
-            creds = StaticCredentialsProvider.create(AwsBasicCredentials.create(keyIdAndSecret.accessKeyId.get(), keyIdAndSecret.accessKeySecret.get()))
-
-            /* roleARN is present, get temporary credentials through AssumeRole */
-            if (keyIdAndSecret.awsRoleARN.isPresent) {
-                creds = getNewCredentialsFromAssumeRole(creds, keyIdAndSecret.awsRoleARN.get())
-            }
-
         } else {
             creds = null
         }
 
-        if(accessMethod.aWSDirect.get().hostname.isPresent) {
+        if(!accessMethod.aWSDirect.get().hostname.isPresent) {
+            return  DynamoDbClient.builder()
+                .region(_awsRegion)
+                .credentialsProvider(creds)
+                .build()
+        } else {
             val aWSDirect = accessMethod.aWSDirect.get()
             val hostname: String = aWSDirect.hostname.get()
             val endpoint = URI(hostname)
@@ -114,11 +118,6 @@ class DynamoDBClient(private val config: DynamoDBDataAccessProviderDataAccessPro
             logger.debug("AWS Direct DbClient Endpoints: {}", dbclient.describeEndpoints().toString())
 
             return dbclient;
-        } else {
-            return  DynamoDbClient.builder()
-                .region(_awsRegion)
-                .credentialsProvider(creds)
-                .build()
         }
     }
 
