8836af509d6accc4048886baf82e3d0bf9677acf
Pedro Felix <pedro.felix@curity.io>
2021-05-26 16:23:28 +0100

    IS- 5626: uses 'debug' level messages when building the query plan

--- src/main/kotlin/io/curity/identityserver/plugin/dynamodb/token/DynamoDBDelegationDataAccessProvider.kt
+++ src/main/kotlin/io/curity/identityserver/plugin/dynamodb/token/DynamoDBDelegationDataAccessProvider.kt
@@ -22,7 +22,6 @@ import io.curity.identityserver.plugin.dynamodb.NumberLongAttribute
 import io.curity.identityserver.plugin.dynamodb.PartitionAndSortIndex
 import io.curity.identityserver.plugin.dynamodb.PartitionOnlyIndex
 import io.curity.identityserver.plugin.dynamodb.PrimaryKey
-import io.curity.identityserver.plugin.dynamodb.SchemaErrorException
 import io.curity.identityserver.plugin.dynamodb.StringAttribute
 import io.curity.identityserver.plugin.dynamodb.Table
 import io.curity.identityserver.plugin.dynamodb.configuration.DynamoDBDataAccessProviderConfiguration
@@ -335,14 +334,15 @@ class DynamoDBDelegationDataAccessProvider(
 
     override fun getAll(resourceQuery: ResourceQuery): Collection<DynamoDBDelegation> = try
     {
-        // A null filter implies a table scan
-        val resourceQueryFilter = resourceQuery.filter ?: throw UnsupportedQueryException.QueryRequiresTableScan()
-
         val comparator = getComparatorFor(resourceQuery)
 
-        val queryPlanner = QueryPlanner(DelegationTable.queryCapabilities)
-
-        val queryPlan = queryPlanner.build(resourceQueryFilter)
+        val queryPlan = if (resourceQuery.filter != null)
+        {
+            QueryPlanner(DelegationTable.queryCapabilities).build(resourceQuery.filter)
+        } else
+        {
+            QueryPlan.UsingScan.fullScan()
+        }
 
         if (queryPlan is QueryPlan.UsingScan && !_configuration.getAllowTableScans().orElse(false))
         {
