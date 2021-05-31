5af15078ffffddfcf5f4b2a1cf2b67db422e8045
Pedro Felix <pedro.felix@curity.io>
2021-05-31 08:12:38 +0100

    Changes copyright headers to apache 2.0

--- src/main/kotlin/io/curity/identityserver/plugin/dynamodb/utils.kt
+++ src/main/kotlin/io/curity/identityserver/plugin/dynamodb/utils.kt
@@ -1,12 +1,17 @@
 /*
- * Copyright (C) 2021 Curity AB. All rights reserved.
+ *  Copyright 2021 Curity AB
  *
- * The contents of this file are the property of Curity AB.
- * You may not copy or use this file, in either source code
- * or executable form, except in compliance with terms
- * set by Curity AB.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * For further information, please contact Curity AB.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package io.curity.identityserver.plugin.dynamodb
@@ -15,7 +20,6 @@ import org.slf4j.LoggerFactory
 import software.amazon.awssdk.services.dynamodb.model.TransactionCanceledException
 import java.security.SecureRandom
 import java.util.Base64
-import java.lang.IllegalArgumentException
 
 private val _logger = LoggerFactory.getLogger("utils")
 
