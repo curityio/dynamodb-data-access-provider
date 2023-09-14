65c72d65900ff9b7377ee5f30618c27f342f9644
Jean-Marc Delâtre <jean-marc.delatre@curity.io>
2023-09-14 14:04:23 +0200

    IS-7807/IS-8005 Take into account new indexes design in preliminary DAP & test

--- src/main/kotlin/io/curity/identityserver/plugin/dynamodb/DynamoDBDatabaseClientDataAccessProvider.kt
+++ src/main/kotlin/io/curity/identityserver/plugin/dynamodb/DynamoDBDatabaseClientDataAccessProvider.kt
@@ -31,54 +31,87 @@ class DynamoDBDatabaseClientDataAccessProvider(
     private val _jsonHandler = _configuration.getJsonHandler()
 
     object DatabaseClientsTable : TableWithCapabilities("curity-database-clients") {
+        // Table Partition Key (PK)
         const val PROFILE_ID = "profileId"
-        const val CLIENT_KEY = "clientKey"
+
+        // Table Sort Key (SK)
+        const val CLIENT_ID_KEY = "clientIdKey"
+
+        // PK for clientName-based GSIs
+        const val CLIENT_NAME_KEY = "clientNameKey"
+
+        // PK for tag-based GSIs
+        const val TAG_KEY = "tagKey"
 
         val profileId = StringAttribute(PROFILE_ID)
         // DynamoDB-specific, composite string made up of clientId and tag, or clientId only
-        val clientKey = StringAttribute(CLIENT_KEY)
+        val clientIdKey = StringAttribute(CLIENT_ID_KEY)
+
+        // DynamoDB-specific, composite string made up of profileId and clientName
+        val clientNameKey = StringAttribute(CLIENT_NAME_KEY)
+
+        // DynamoDB-specific, composite string made up of profileId and an individual item from tags
+        val tagKey = StringAttribute(CLIENT_NAME_KEY)
         val clientName = StringAttribute("clientName")
         val created = NumberLongAttribute("created")
         val updated = NumberLongAttribute("updated")
         val status = StringAttribute("status")
-        // DynamoDB-specific field for indexes
-        val tag = StringAttribute("tag")
         val tags = ListStringAttribute("tags")
 
-        val primaryKeys = PrimaryKeys(profileId, clientKey)
-        private val clientNameCreatedIndex =
-            PartitionAndSortIndex("clientName-created-index", clientName, created)
-        private val clientNameUpdatedIndex =
-            PartitionAndSortIndex("clientName-updated-index", clientName, updated)
-        private val tagCreatedIndex =
-            PartitionAndSortIndex("tag-created-index", tag, created)
-        private val tagUpdatedIndex =
-            PartitionAndSortIndex("tag-updated-index", tag, updated)
-        private val tagClientNameIndex =
-            PartitionAndSortIndex("tag-clientName-index", tag, clientName)
+        val primaryKeys = PrimaryKeys(profileId, clientIdKey)
 
-        override fun queryCapabilities(): TableQueryCapabilities = TableQueryCapabilities(
+        // GSIs
+        private val clientNameCreatedIndex =
+            PartitionAndSortIndex("clientName-created-index", clientNameKey, created)
+        private val clientNameUpdatedIndex =
+            PartitionAndSortIndex("clientName-updated-index", clientNameKey, updated)
+        private val clientNameClientNameIndex =
+            PartitionAndSortIndex("clientName-clientName-index", clientNameKey, clientName)
+        private val tagCreatedIndex =
+            PartitionAndSortIndex("tag-created-index", tagKey, created)
+        private val tagUpdatedIndex =
+            PartitionAndSortIndex("tag-updated-index", tagKey, updated)
+        private val tagClientNameIndex =
+            PartitionAndSortIndex("tag-clientName-index", tagKey, clientName)
+
+        // LSIs
+        private val lsiCreatedIndex =
+            PartitionAndSortIndex("lsi-created-index", profileId, created)
+        private val lsiUpdatedIndex =
+            PartitionAndSortIndex("lsi-updated-index", profileId, updated)
+        private val lsiClientNameIndex =
+            PartitionAndSortIndex("lsi-clientName-index", profileId, clientName)
+
+        override fun queryCapabilities(): TableQueryCapabilities = object : TableQueryCapabilities(
             indexes = listOf(
                 Index.from(primaryKeys),
                 Index.from(clientNameCreatedIndex),
                 Index.from(clientNameUpdatedIndex),
+                Index.from(clientNameClientNameIndex),
                 Index.from(tagCreatedIndex),
                 Index.from(tagUpdatedIndex),
                 Index.from(tagClientNameIndex),
+                Index.from(lsiCreatedIndex),
+                Index.from(lsiUpdatedIndex),
+                Index.from(lsiClientNameIndex),
             ),
             attributeMap = mapOf(
                 "profile_id" to profileId,
-                DatabaseClientAttributeKeys.CLIENT_ID to clientKey,
+                DatabaseClientAttributeKeys.CLIENT_ID to clientIdKey,
+                "clientNameKey" to clientNameKey,
                 DatabaseClientAttributeKeys.NAME to clientName,
-                "tag" to tag,
+                "tagKey" to tagKey,
                 DatabaseClientAttributeKeys.TAGS to tags,
                 Meta.CREATED to created,
                 Meta.LAST_MODIFIED to updated,
-                DatabaseClientAttributeKeys.STATUS to status
+                DatabaseClientAttributeKeys.STATUS to status,
             )
-        )
+        ) {
+            override fun getGsiCount() = 6
+            override fun getLsiCount() = 3
+        }
 
-        override fun keyAttribute(): StringAttribute = clientKey
+        override fun keyAttribute(): StringAttribute = clientIdKey
 
         fun key(value: String) = mapOf(keyAttribute().toNameValuePair(value))
     }
